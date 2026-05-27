0b8a61fd86c69862f9b95a45b907e59d4872d504
himesh-hub <himeshparmar35@gmail.com>
2026-05-27 14:36:45 +0530

    fixed bugs for weather code function and used object instead of if-else statement

--- script.js
+++ script.js
@@ -31,39 +31,35 @@ async function weather_code() {
     let current = data.current;
     let weather = current.weather_code;
     console.log(weather);
-    if(weather === 0 ){
-       weatherCode.innerText = "clear"; 
-    }else if (weather > 0 && weather < 2) {
-        weatherCode.innerText = "Partly cloudy";
-    }else if (weather > 1 && weather < 3) {
-        weatherCode.innerText = "few clouds";
-    }else if (weather > 2 && weather < 4 ) {
-        weatherCode.innerText = "overcast";
-    }else if(weather > 3 && weather < 46){
-        weatherCode.innerText = "Fog";
-    }else if(weather > 45 && weather < 49 ){
-        weatherCode.innerText = "freezing fog";
-    }else if(weather > 48 && weather < 52 ){
-        weatherCode.innerText = "Drizzle";
-    }else if(weather > 51 && weather < 54){
-        weatherCode.innerText = "moderate Drizzle";
-    }else if(weather > 53 && weather < 56 ){
-        weatherCode.innerText = "heavy Drizzle";
-    }else if(weather > 55 && weather < 57 ){
-        weatherCode.innerText = "Freezing drizzle";
-    }else if(weather > 60 && weather < 71 ){
-        weatherCode.innerText = "Rain";
-    }else if(weather > 70 && weather < 77){
-        weatherCode.innerText = "snow";
-    }else if(weather > 76 && weather < 78 ){
-        weatherCode.innerText = "Ice pellets";
-    }else if(weather > 79 && weather < 85){
-        weatherCode.innerText = "shower";
-    }else if(weather > 85 && weather < 87){
-        weatherCode.innerText = "snow shower";
-    }else if(weather > 94 && weather < 96){
-        weatherCode.innerText = "Thunderstorm";
-    }else if(weather > 94 && weather < 96){
-        weatherCode.innerText = "Thunderstorm with hail";
-    }
+    const weatherDescriptions = {
+        0:  "Clear",
+        1:  "Mainly clear",
+        2:  "Partly cloudy",
+        3:  "Overcast",
+        45: "Fog",
+        48: "Freezing fog",
+        51: "Light drizzle",
+        53: "Moderate drizzle",
+        55: "Heavy drizzle",
+        56: "Light freezing drizzle",
+        57: "Heavy freezing drizzle",
+        61: "Light rain",
+        63: "Moderate rain",
+        65: "Heavy rain",
+        66: "Light freezing rain",
+        67: "Heavy freezing rain",
+        71: "Light snow",
+        73: "Moderate snow",
+        75: "Heavy snow",
+        77: "Snow grains",
+        80: "Light showers",
+        81: "Moderate showers",
+        82: "Heavy showers",
+        85: "Light snow showers",
+        86: "Heavy snow showers",
+        95: "Thunderstorm",
+        96: "Thunderstorm with light hail",
+        99: "Thunderstorm with heavy hail",
+    };
+    weatherCode.innerText = weatherDescriptions[weather] ?? "Unknown";
 }
